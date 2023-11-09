51abf25930f7b57bfe06c33ebfddb7e9cdd350d1
Manuel <manejo03@outlook.es>
2023-11-08 22:33:28 -0500

    Trabajo Final

---
+++ Assets/Js/login.js
@@ -0,0 +1,17 @@
+function iniciarSesion() {
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    const correo = email.value;
+    const contraseña = password.value;
+
+  if (correo !== "" && contraseña !== "") {
+      alert("Ha Iniciado Sesión Exitosamente");
+      
+      email.value = "";
+      password.value = "";
+
+  } else {
+    alert("LLene los Campos Faltantes")
+  }
+}
